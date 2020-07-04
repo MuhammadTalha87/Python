fb87c618465d2e871013d8d12617b366682270a0
Muhammad <muhammad.talha@stud.hslu.ch>
2020-07-04 13:35:55 +0200

    Strings are added to the example

--- TypeConversion.py
+++ TypeConversion.py
@@ -1,4 +1,14 @@
 
+#String code is below
+lang = "Hello" 
+print print(lang[0:4])  #this will show only hell
+print print(lang[0])    #this will show only H
+print(lang[:])          #this will show whole string
+
+
+#This is type conversion code
 weight_pounds = int(input("Enter your weight in pounds : "))
 weight_kgs=weight_pounds*0.45
 print(weight_kgs)
+
+
