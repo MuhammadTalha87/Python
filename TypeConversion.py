048481a753a6fa68a45748d075590a8b6cbf36af
Muhammad <muhammad.talha@stud.hslu.ch>
2020-07-04 14:19:59 +0200

    string functions are covered

--- TypeConversion.py
+++ TypeConversion.py
@@ -1,9 +1,25 @@
 
 #String code is below
 lang = "Hello" 
-print print(lang[0:4])  #this will show only hell
-print print(lang[0])    #this will show only H
+print(lang[0:4])  #this will show only hell
+print(lang[0])    #this will show only H
 print(lang[:])          #this will show whole string
+print(lang[:4])         #this will assume starting index from 0 and will print all characters till hell
+print(lang[1:-1])       #this will show all characters from second index to the second last index.
+second_str = lang[:]    #this will copy whole string in second_str variable
+print(second_str)
+
+first_name ="MUHAMMAD"
+last_name = "talha"
+age = 24
+
+print(first_name.title())  #this will print only first character of name as capital
+print(first_name.find("A")) #this will print only index of character A
+print(first_name.replace("MUHAMMAD", "Talha")) #this will replace MUHAMMAD with Talha
+print("HAMMAD" in first_name)   #this will produce a boolean value which will show if these characters exist or not in first_name
+
+msg = f"{first_name} {last_name} is " +str(age)+ " years old"  #concatenation of a variable type int and Strings
+print(msg)
 
 
 #This is type conversion code
