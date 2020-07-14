52f878b7fa519fb1b2afdf1960241a913112b9b1
Muhammad <muhammad.talha@stud.hslu.ch>
2020-07-14 17:42:49 +0200

    Classes training is done

---
+++ Classes.py
@@ -0,0 +1,26 @@
+class Dog:
+    
+    def __init__(self,breed):
+        self.breed = breed
+
+    def bark(self):
+        print("howwww bhowwww")
+    
+    def intro_of_dog(self):
+        print(f"This is {self.breed}")
+    
+    #def color(self):
+    #    print("Red")
+    
+    
+    #def manufactured_year(self):
+    #    print("2010")
+
+maxi = Dog("German Shephered")
+russian = Dog ("Bull dog")
+
+maxi.intro_of_dog()
+maxi.bark()
+russian.intro_of_dog()
+russian.bark()
+    
